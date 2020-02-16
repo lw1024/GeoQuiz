3c211de84d0be54641a3cfcbb93094f133053a1a
Luwei <lw1023@msn.com>
2020-02-16 22:03:53 +0800

    create secend activity

--- app/src/main/java/xyz/lw1024/geoquiz/MainActivity.java
+++ app/src/main/java/xyz/lw1024/geoquiz/MainActivity.java
@@ -2,10 +2,13 @@ package xyz.lw1024.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "xyz.lw1024.geoquiz.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
