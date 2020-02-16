fdead3fe5b3de4675d75d8d30be95cbb3e5f70da
Luwei <lw1023@msn.com>
2020-02-16 21:45:40 +0800

    init

---
+++ app/src/main/java/xyz/lw1024/geoquiz/MainActivity.java
@@ -0,0 +1,20 @@
+package xyz.lw1024.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
